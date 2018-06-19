71c583877aab6e84973669a3145496651396a9b2
Dustin Whitney <dustin.whitney@gmail.com>
2018-06-19 01:20:14 -0400

    upgraded to purescript 0.12

--- test/Main.purs
+++ test/Main.purs
@@ -2,42 +2,33 @@ module Test.Main where
 
 import Prelude
 
-import Control.Monad.Aff.AVar (AVAR)
-import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Timer (TIMER)
 import Data.Maybe (Maybe(..), fromJust)
 import Data.String (length)
-import Data.UUID (parseUUID, genUUID, GENUUID)
-import Node.Process (PROCESS)
+import Data.UUID (genUUID, parseUUID)
+import Effect (Effect)
+import Effect.Unsafe (unsafePerformEffect)
 import Partial.Unsafe (unsafePartial)
 import Test.Spec (it, describe)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (run)
 
-main :: forall e.
-  Eff
-    ( uuid :: GENUUID
-    , console :: CONSOLE
-    , process :: PROCESS
-    , timer :: TIMER
-    , avar :: AVAR
-    | e
-    ) Unit
-main = unsafePartial $ run [consoleReporter] do
-  describe "Data.UUID" do
+main :: Effect Unit
+main = run [consoleReporter] do
+  
+  describe "UUID" do
+
     it "`genUUID` returns a uuid" do
-      uuid <- liftEff genUUID
+      let uuid = unsafePerformEffect genUUID
       (length <<< show) uuid `shouldEqual` 36
 
     it "`parseUUID` parses a valid uuid as Just UUID" do
       let uuidStr = "d0778cf2-3a4c-42ef-acbd-1269b6bec204"
           uuid = parseUUID uuidStr
-      uuidStr `shouldEqual` (show $ fromJust uuid)
+      uuidStr `shouldEqual` (show $ unsafePartial $ fromJust uuid)
 
     it "`parseUUID` parses an invalid uuid as Nothing" do
       let uuidStr = "foo"
           uuid = parseUUID uuidStr
       Nothing `shouldEqual` uuid
+
