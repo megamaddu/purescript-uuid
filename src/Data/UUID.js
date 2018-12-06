bd83bf2b292a28e9bb88a0ac1703630ae82e0f32
Simon Hafner <hafnersimon@gmail.com>
2018-12-06 20:04:09 +0100

    Added v3 and v5 (#7)
    Also updated to new require format: https://www.npmjs.com/package/uuid

--- src/Data/UUID.js
+++ src/Data/UUID.js
@@ -2,10 +2,26 @@
 
 // module Data.UUID
 
+var mkV3UUID = null;
+exports.getUUID3Impl = function (str) {
+  return function (namespace) {
+    if (mkV3UUID === null) mkV3UUID = require('uuid/v3');
+    return mkV3UUID(str, namespace);
+  };
+};
+
 var mkV4UUID = null;
 exports.getUUIDImpl = function () {
-  if (mkV4UUID === null) mkV4UUID = require('uuid').v4;
+  if (mkV4UUID === null) mkV4UUID = require('uuid/v4');
   return mkV4UUID();
+};
+
+var mkV5UUID = null;
+exports.getUUID5Impl = function (str) {
+  return function (namespace) {
+    if (mkV5UUID === null) mkV5UUID = require('uuid/v5');
+    return mkV5UUID(str, namespace);
+  };
 };
 
 var validateV4UUID = null;
