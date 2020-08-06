1fe102c6732221bf5fc3242f50dbcb89917183c8
Stanislas Signoud <stanislas.signoud@fretlink.com>
2020-08-06 18:35:04 +0200

    Bump uuid from 3.4.0 to 8.3.0
    This bump removes a DeprecationWarning emitted by the new `uuid`
    npm package. It also removes the need to rely on the
    `uuid-validate` as this feature is now embedded in `uuid`.
    
    Closes #16.

--- src/Data/UUID.js
+++ src/Data/UUID.js
@@ -5,27 +5,39 @@
 var mkV3UUID = null;
 exports.getUUID3Impl = function (str) {
   return function (namespace) {
-    if (mkV3UUID === null) mkV3UUID = require('uuid/v3');
+    if (mkV3UUID === null) {
+      var uuid = require('uuid');
+      mkV3UUID = uuid.v3;
+    }
     return mkV3UUID(str, namespace);
   };
 };
 
 var mkV4UUID = null;
 exports.getUUIDImpl = function () {
-  if (mkV4UUID === null) mkV4UUID = require('uuid/v4');
+  if (mkV4UUID === null) {
+    var uuid = require('uuid');
+    mkV4UUID = uuid.v4;
+  }
   return mkV4UUID();
 };
 
 var mkV5UUID = null;
 exports.getUUID5Impl = function (str) {
   return function (namespace) {
-    if (mkV5UUID === null) mkV5UUID = require('uuid/v5');
+    if (mkV5UUID === null) {
+      var uuid = require('uuid');
+      mkV5UUID = uuid.v5;
+    }
     return mkV5UUID(str, namespace);
   };
 };
 
-var validateV4UUID = null;
+var validateUUID = null;
 exports.validateV4UUID = function (str) {
-  if (validateV4UUID === null) validateV4UUID = require('uuid-validate');
-  return validateV4UUID(str, 4);
+  if (validateUUID === null) {
+    var uuid = require('uuid');
+    validateUUID = uuid.validate;
+  }
+  return validateUUID(str);
 };
