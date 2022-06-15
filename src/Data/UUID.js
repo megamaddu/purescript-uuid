b85957ef57d4d8d9c3aa21e93bf6a5ea28b8b31d
Kari Pahula <kaol@iki.fi>
2022-06-15 13:57:22 +0300

    Import uuid library using module object syntax

--- src/Data/UUID.js
+++ src/Data/UUID.js
@@ -1,18 +1,18 @@
 // module Data.UUID
-import { v3, v4, v5, validate } from "uuid";
+import uuid from "uuid";
 
 export const getUUID3Impl = function (str) {
   return function (namespace) {
-    return v3(str, namespace);
+    return uuid.v3(str, namespace);
   };
 };
 
-export const getUUIDImpl = v4;
+export const getUUIDImpl = uuid.v4;
 
 export const getUUID5Impl = function (str) {
   return function (namespace) {
-    return v5(str, namespace);
+    return uuid.v5(str, namespace);
   };
 };
 
-export const validateV4UUID = validate;
+export const validateV4UUID = uuid.validate;
